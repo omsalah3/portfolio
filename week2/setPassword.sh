64addf89ec568a9a67dd01db37b644b18ab2d08c
Ubuntu <student@ML-RefVm-511909.ptujgdsgokie5k0135u1gclfzb.px.internal.cloudapp.net>
2023-01-27 04:19:11 +0000

    setPassword.sh

--- week2/setPassword.sh
+++ week2/setPassword.sh
@@ -6,10 +6,9 @@ read folder
 #enter the secret password
 read -rsp "Enter the secret password:" password
 
-#save the password in the secret.txt file in the chosen folder
-if [ ! -d "$folder" ]; then
-  mkdir $folder
-fi
+# make a folder as the user shose
+mkdir $folder
 
+#save the password in the secret.txt in the chosen folder
 echo $password > $folder/secret.txt
 echo "password saved in $folder/secret.txt"
